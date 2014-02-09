b93299c1bf8555db985180444779b5746ee07515
Roberto Cadena Vega <robblack00_7@hotmail.com>
2014-02-09 17:58:26 -0600

    Se hace un script que lee el voltaje en una resistencia y prende un LED con la intensidad reportada

---
+++ analoginput.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from Adafruit_BBIO import ADC, PWM
+from math import sin, pi
+import time
+
+ADC.setup()
+PWM.start("P8_13", 0)
+
+while True:
+	lectura = ADC.read("P9_39")
+	PWM.set_duty_cycle("P8_13", lectura)
