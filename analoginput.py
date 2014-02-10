4225edd7dd27c6e654eb7ff43f7b4dc7b3a33d9d
Roberto Cadena Vega <robblack00_7@hotmail.com>
2014-02-09 18:03:49 -0600

    Se convierte indentacion a espacios :unamused:

--- analoginput.py
+++ analoginput.py
@@ -8,5 +8,5 @@ ADC.setup()
 PWM.start("P8_13", 0)
 
 while True:
-	lectura = ADC.read("P9_39")
-	PWM.set_duty_cycle("P8_13", lectura)
+    lectura = ADC.read("P9_39")
+    PWM.set_duty_cycle("P8_13", lectura)
